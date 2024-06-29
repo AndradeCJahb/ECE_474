5345a2c28cf8c4b76ccfdc5370c734ef9ed9021e
Christopher Andrade <andralug01@gmail.com>
2024-06-28 17:41:26 -0700

    ALF folder fix

--- lab2/Lab2Part1Step3DRA/Lab2Part1Step3DRA.ino
+++ lab2/Lab2Part1Step3DRA/Lab2Part1Step3DRA.ino
@@ -11,7 +11,7 @@
 
 void setup() {
   PIN_FUNC_SELECT(GPIO_PIN_MUX_REG[GPIO_PIN], PIN_FUNC_GPIO);           // set pin 5 (D2) to be a general purpose input/output
-  *((volatile uint32_t*) GPIO_ENABLE_REG) |= (1 << GPIO_PIN);            // set pin 5 (D2) to be an output by moving a '1' into bit 5 of enable register
+  *((volatile uint32_t*) GPIO_ENABLE_REG) |= (1 << GPIO_PIN);           // set pin 5 (D2) to be an output by moving a '1' into bit 5 of enable register
 }
 
 
