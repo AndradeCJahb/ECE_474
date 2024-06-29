3065fac23459f99f11622b3bb4d82d9f4b6068ad
Christopher Andrade <andralug01@gmail.com>
2024-06-29 02:31:51 -0700

    comment fix

--- lab2/Lab2Part2/Lab2Part2.ino
+++ lab2/Lab2Part2/Lab2Part2.ino
@@ -1,16 +1,16 @@
-// Filename:    Lab2Part2
+// Filename:    Lab2Part2.ino
 // Author:      Christopher Andrade (2221525), Theo Favour (2169814)
 // Date:        06/25/24
-// Description: 
+// Description: This file toggles an off-board LED on and off at 1 second intervals directly accessing TIMG and GPIO registers
 
 #include "driver/gpio.h"
 #include "soc/io_mux_reg.h"
 #include "soc/gpio_reg.h"
 #include "soc/timer_group_reg.h"
 
-#define LED_PIN 5 // LED connected to D2
+#define LED_PIN 5                       // LED connected to D2
 
-unsigned long lastToggleTime = 0; // Last time the LED was toggled
+unsigned long lastToggleTime = 0;       // Last time the LED was toggled
 const unsigned long interval = 1000000; // 1 second in microseconds
 
 void setup() {
@@ -18,10 +18,9 @@ void setup() {
  PIN_FUNC_SELECT(GPIO_PIN_MUX_REG[LED_PIN], PIN_FUNC_GPIO);
  *((volatile uint32_t*) GPIO_ENABLE_REG) |= (1 << LED_PIN);
 
- *((volatile uint32_t*) GPIO_OUT_REG) &= ~(1 << LED_PIN); // Ensure the LED is off to start
+ *((volatile uint32_t*) GPIO_OUT_REG) &= ~(1 << LED_PIN);               // Ensure the LED is off to start
 
  *((volatile uint32_t*) TIMG_T0CONFIG_REG(0)) &= 0x00000000;
-
  *((volatile uint32_t*) TIMG_T0CONFIG_REG(0)) |= 1110000000000000011 << 13;
 }
 void loop() {
@@ -30,7 +29,7 @@ void loop() {
  
   // Check if the interval has passed
   if (currentTime - lastToggleTime >= interval) {
-    *((volatile uint32_t*) GPIO_OUT_REG) ^= (1 << LED_PIN); // Toggle the LED state
-    lastToggleTime = currentTime; // Update the last toggle time
+    *((volatile uint32_t*) GPIO_OUT_REG) ^= (1 << LED_PIN);             // Toggle the LED state
+    lastToggleTime = currentTime;                                       // Update the last toggle time
   }
 }
