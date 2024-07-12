699e6527a9ebd5139129359462d014d277a4ad40
Christopher Andrade <andralug01@gmail.com>
2024-07-12 10:31:53 -0700

    icte 6 final

--- ICTE6/ICTE6_CANDRADE/ICTE6_CANDRADE.ino
+++ ICTE6/ICTE6_CANDRADE/ICTE6_CANDRADE.ino
@@ -1,22 +1,23 @@
-// Filename:    ICTE6_CANDRADE.ino
+// Filename:    ICTE6_Lab1.ino
 // Author:      Christopher Andrade (2221525), Theo Favour (2169814)
 // Date:        07/10/2024
 // Description: This file utilizes an off-board tactile switch and interrupts to control the status of an off-board LED
 //              (If the button is pressed the LED lights, LED is off otherwise)
 
-int state = 0;
+int state = 0;                                                    // State which is written to the D2 LED pin
 
 void setup() {
   pinMode (D3, INPUT_PULLUP);                                     // initialize Port D3 as a "PULLED UP" input
   pinMode (D2, OUTPUT);                                           // initialize Port D2 as output
-  attachInterrupt(digitalPinToInterrupt(D3), toggle, CHANGE);
-  digitalWrite(D2, 0);
+  attachInterrupt(digitalPinToInterrupt(D3), toggle, CHANGE);     // Attach interrupt to changes in D3 pin
+  digitalWrite(D2, 0);                                            // Ensure D2 pin starts LOW/0
 }
 
 void loop() {
-  digitalWrite(D2, state);
+  digitalWrite(D2, state);                                        // constantly write the state variable to LED (1 or 0, HIGH or LOW)
 }
 
+// Description: Function that toggles state variable on interrupt (change in D3)
 void toggle() {
-  state = !state;
+  state = !state;                                                 // Toggle state variable
 }
